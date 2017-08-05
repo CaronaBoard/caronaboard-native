49199b6e2c1acf22d89c80c56caa91a144f02b16
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-05 20:51:17 -0300

    Testing Danger Message

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,13 +1,4 @@
-// # Sometimes it's a README fix, or something like that - which isn't relevant for
-// # including in a project's CHANGELOG for example
-declared_trivial = github.pr_title.include? "#trivial"
-
-// # Make it more obvious that a PR is a work in progress and shouldn't be merged yet
-warn("PR is classed as Work in Progress") if github.pr_title.include? "[WIP]"
-
-// # Warn when there is a big PR
-warn("Big PR") if git.lines_of_code > 500
-
-// # Don't let testing shortcuts get into master by accident
 fail("fdescribe left in tests") if `grep -r fdescribe __tests__/ `.length > 1
 fail("fit left in tests") if `grep -r fit __tests__/ `.length > 1
+
+message("<table> <tr> <td> <a href='http://bit.ly/cb-appetize'> <p align='center'> <img height='130' src='http://bit.ly/2vD4vJg'/> <br/> Nexus 5 </p></a> </td></tr></table>")
