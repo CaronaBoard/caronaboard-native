5adc3a2b3bf507b6f86ca1f3a15664b235d57d63
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-05 21:05:25 -0300

    Last chance

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,4 +1,3 @@
-fail("fdescribe left in tests") if `grep -r fdescribe __tests__/ `.length > 1
-fail("fit left in tests") if `grep -r fit __tests__/ `.length > 1
+import { message } from "danger"
 
 message("<table> <tr> <td> <a href='http://bit.ly/cb-appetize'> <p align='center'> <img height='130' src='http://bit.ly/2vD4vJg'/> <br/> Nexus 5 </p></a> </td></tr></table>")
