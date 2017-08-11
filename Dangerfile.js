a07d702263e04178830e00b1a2f1857649d3b27b
Eduardo Moroni <eduardomoronI@gmail.com>
2017-08-11 09:36:53 -0300

    Showing device grids on PR with Danger (#35)
    * Danger Test
    
    * Travis uses yarn instead npm
    
    * Revert "Travis uses yarn instead npm"
    
    This reverts commit d5e3952f402cac3c36a353d3309c891b7ecdaae4.
    
    * Nao funciona
    
    * Danger only works in Travis/pr execution
    
    * Testing Danger Message
    
    * Last chance
    
    * Danger is commenting static html table poiting to appetize
    
    * Removing log file
    
    * Improving fastlane log and travis running order

---
+++ Dangerfile.js
@@ -0,0 +1,3 @@
+import { message } from "danger"
+
+message("<table> <tr> <td> <a href='http://bit.ly/cb-appetize'> <p align='center'> <img height='130' src='http://bit.ly/2vD4vJg'/> <br/> Nexus 5 </p></a> </td></tr></table>")
