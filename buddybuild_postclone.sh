2a0aa4108769cac30c89b74ee831bb6c705e9fe9
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-22 09:52:30 -0300

    Test exporting .env on buddybuild

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 export ENVFILE="${BUDDYBUILD_SECURE_FILES}/.env"
-echo "buddybuild workspace folder is ${BUDDYBUILD_WORKSPACE}."
-echo "exported ENVFILE to ${ENVFILE}."
-echo "echoing files in ${BUDDYBUILD_SECURE_FILES}/"
+echo "export ENVFILE=\"${BUDDYBUILD_SECURE_FILES}/.env\"" >> $HOME/.bashrc
+# echo "buddybuild workspace folder is ${BUDDYBUILD_WORKSPACE}."
+# echo "exported ENVFILE to ${ENVFILE}."
 
-for file in ${BUDDYBUILD_SECURE_FILES}/*; do
-  echo ${file##*/}
-done
+# echo "Creating environment config file"
+
+# #TODO: This is just a POC, this secrets will be deleted after hack-night
+# touch $ENVFILE
+# tee $ENVFILE > /dev/null <<EOF
+# API_KEY=AIzaSyBQnTKUKjQ0HkLUdHVs7a8_hFyCO-nbT-M
+# AUTH_DOMAIN=caronaboard-hack-night.firebaseapp.com
+# DATABASE_URL=https://caronaboard-hack-night.firebaseio.com
+# STORAGE_BUCKET=caronaboard-hack-night.appspot.com
+# MESSAGING_SENDER_ID=905421185910
+# EOF
