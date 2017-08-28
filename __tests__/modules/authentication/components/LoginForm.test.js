928be152bcd73a9c62f4786a6735f52669f61fad
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2017-08-28 20:41:02 -0300

    [@OtavioRMachado, @henriquelbsouza] Refactored to be possible to use email in another login screen

--- __tests__/modules/authentication/components/LoginForm.test.js
+++ __tests__/modules/authentication/components/LoginForm.test.js
@@ -1,4 +1,3 @@
-import { View } from 'react-native'
 import React from 'react'
 import { shallow } from 'enzyme'
 import { LoginForm } from '../../../../src/modules/authentication/components/LoginForm'
