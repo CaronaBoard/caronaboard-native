267ace4cd9aa40dcb1bf56c44338e88acec97463
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-24 07:33:37 -0300

    refactor: refactor signin footer

---
+++ src/modules/authentication/components/SignInFooter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import PropTypes from 'prop-types'
+import { RkButton, RkText } from 'react-native-ui-kitten'
+import Styles from './styles/SignInFooterStyles'
+
+export class SignInFooter extends Component {
+  static propTypes = {
+    email: PropTypes.string,
+    navigator: PropTypes.object.isRequired
+  }
+
+  navigateTo = (screen) => () => this.props.navigator.push({screen})
+
+  render () {
+    return (
+      <View>
+        <View style={Styles.textRow}>
+          <RkButton rkType='clear' onPress={this.navigateTo('carona.forgotPassword')}>
+            <RkText rkType='header6'>Forgot your password?</RkText>
+          </RkButton>
+        </View>
+        <View style={Styles.textRow}>
+          <RkText rkType='primary3'>Don’t have an account?</RkText>
+          <RkButton rkType='clear' onPress={this.navigateTo('carona.signUp')}>
+            <RkText rkType='header6'> Sign up now</RkText>
+          </RkButton>
+        </View>
+      </View>
+    )
+  }
+}
