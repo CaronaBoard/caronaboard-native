68184de7a6d8963a5a22fdda6501baed51986cb8
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-28 19:22:33 -0300

    feat: add alert for removing ride offer

--- src/modules/rideOffer/containers/YourRideOffersScreen.js
+++ src/modules/rideOffer/containers/YourRideOffersScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { FlatList, View } from 'react-native'
+import { FlatList, View, Alert } from 'react-native'
 import { RideOffer } from '../components/RideOffer'
 import { getUserRideOffers } from '../../../services/firebase/database/RideOffer'
 import { FloatingActionButton } from '../../shared/components/FloatingActionButton'
@@ -28,13 +28,25 @@ export class YourRideOffersScreen extends Component {
     this.props.navigator.push({screen: screens.rideOffer.id})
   }
 
+  onPressRide = (rideId: string) => {
+    Alert.alert(
+      'Delete Ride Offer',
+      'Just a confirmation whether you wanna delete this ride or not.',
+      [
+        {text: 'Sure thing, delete!', style: 'destructive', onPress: () => null},
+        {text: 'Not yet, Thanks', style: 'cancel'}
+      ],
+      { cancelable: true }
+    )
+  }
+
   render () {
     return (
       <View style={{flex: 1}}>
         <FlatList
           data={this.state.rides}
           keyExtractor={item => item.rideId}
-          renderItem={({ item }) => (<RideOffer ride={item} />)}
+          renderItem={({ item }) => (<RideOffer ride={item} onPress={this.onPressRide} />)}
         />
         <FloatingActionButton onPress={() => this.pushRideRequestScreen()} />
       </View>
