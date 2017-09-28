10fa6b381ff651ae7217793aa181f32482456b24
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-28 09:56:09 -0300

    test(AuthReducer): add SAVE_PROFILE_FIREBASE test

--- __tests__/redux/reducers/AuthReducers.test.js
+++ __tests__/redux/reducers/AuthReducers.test.js
@@ -1,5 +1,5 @@
 import AuthReducer, { INITIAL_STATE } from '../../../src/redux/reducers/AuthReducers'
-import { signUpFirebase } from '../../../src/redux/actions/AuthActions'
+import { signUpFirebase, saveProfileFirebase } from '../../../src/redux/actions/AuthActions'
 import { USER_SIGNED_UP } from '../../../src/services/message'
 import { extractActionFromThunk } from '../../__mocks__/ReduxThunkMock'
 import * as FirebaseService from '../../../src/services/firebase'
@@ -7,6 +7,8 @@ import * as FirebaseService from '../../../src/services/firebase'
 jest.mock('../../../src/services/firebase')
 
 describe('AuthReducer', () => {
+  const userId = 'vegeta'
+
   it('Should handle successfull sign up', async () => {
     FirebaseService.signUp = jest.fn(() => Promise.resolve())
 
@@ -24,6 +26,24 @@ describe('AuthReducer', () => {
     const action = await extractActionFromThunk(signUpFirebase, 'email', 'password')
     const state = AuthReducer(INITIAL_STATE, action)
     const expectedState = { ...state, alert: {showAlert: true, message: action.payload} }
+
+    expect(state).toEqual(expectedState)
+  })
+
+  it('Should update user profile', async () => {
+    const profile = {
+      name: 'duduzinho',
+      contact: {
+        kind: 'carta',
+        value: 'Rua dos bobos, Numero zero'
+      }
+    }
+
+    FirebaseService.saveProfile = jest.fn(() => Promise.resolve(profile))
+
+    const action = await extractActionFromThunk(saveProfileFirebase, profile, userId)
+    const state = AuthReducer(INITIAL_STATE, action)
+    const expectedState = { ...state, profile: action.profile }
 
     expect(state).toEqual(expectedState)
   })
