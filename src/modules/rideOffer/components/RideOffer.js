27179064ae1e69e935127de15836b75cc9dfdda0
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-28 16:06:50 -0300

    feat: style ride offer component

--- src/modules/rideOffer/components/RideOffer.js
+++ src/modules/rideOffer/components/RideOffer.js
@@ -1,9 +1,41 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import Styles from './styles/RideOfferStyles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import { RkCard } from 'react-native-ui-kitten'
 import { ridePropTypes } from '../../rideRequest/types'
 
-export const RideOffer = props => {
-  console.log(props, 'is ===> props')
+const renderLine = (text, icon) => {
+  return (
+    <View style={Styles.line} >
+      <Icon name={icon} style={Styles.icon} />
+      <Text rkCardText>{text}</Text>
+    </View>
+  )
+}
 
-  return null
+export const RideOffer = props => {
+  const {
+    origin,
+    destination,
+    days,
+    hours
+  } = props.ride
+
+  return (
+    <TouchableOpacity>
+      <RkCard>
+        <View rkCardContent>
+          { renderLine(origin, 'radio-button-unchecked') }
+          { renderLine(destination, 'radio-button-unchecked') }
+        </View>
+        <View rkCardContent>
+          { renderLine(days, 'today') }
+          { renderLine(hours, 'schedule') }
+        </View>
+      </RkCard>
+    </TouchableOpacity>
+  )
 }
 
 RideOffer.propTypes = {
