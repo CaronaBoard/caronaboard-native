298c9d69dfb3bce09da2c1ed8f91a96f23e8934c
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-28 08:20:21 -0300

    refactor: type checking firebase conversion

--- src/services/firebase/Conversion.js
+++ src/services/firebase/Conversion.js
@@ -1,6 +1,8 @@
+// @flow
 import _ from 'lodash'
+import type { rideType } from './types'
 
-export const toArrayOfRides = (firebaseResponse) => {
+export const toArrayOfRides = (firebaseResponse: any): rideType => {
   return _.flatMap(firebaseResponse, (value, driverId) => {
     const ridesIds = Object.keys(value)
     const rides = Object.values(value)
---
+++ src/services/firebase/types.js
@@ -0,0 +1,15 @@
+export type rideType = {
+  days: string,
+  destination: string,
+  hours: string,
+  origin: string,
+  driverId: string,
+  rideId: string,
+  profile: {
+    name: string,
+    contact: {
+      kind: string,
+      value: string
+    }
+  }
+}
