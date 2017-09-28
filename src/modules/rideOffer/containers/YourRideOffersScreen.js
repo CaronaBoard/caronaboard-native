4eac6bf1ab4c6e99c62f256dbae45d782cbc9e2e
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-28 16:51:51 -0300

    feat: navigate to ride offer through FAB

--- src/modules/rideOffer/containers/YourRideOffersScreen.js
+++ src/modules/rideOffer/containers/YourRideOffersScreen.js
@@ -5,6 +5,7 @@ import { FlatList, View } from 'react-native'
 import { RideOffer } from '../components/RideOffer'
 import { getUserRideOffers } from '../../../services/firebase/database/RideOffer'
 import { FloatingActionButton } from '../../shared/components/FloatingActionButton'
+import { screens } from '../../../navigation/Screens'
 
 export const INITIAL_STATE = {
   rides: []
@@ -16,13 +17,16 @@ export class YourRideOffersScreen extends Component {
     profile: PropTypes.object.isRequired
   }
 
+  state = INITIAL_STATE
+
   componentDidMount = async () => {
     const rides = await getUserRideOffers(this.props.profile.uid)
-    console.log(rides, 'is ===> rides')
     this.setState({rides})
   }
 
-  state = INITIAL_STATE
+  pushRideRequestScreen () {
+    this.props.navigator.push({screen: screens.rideOffer.id})
+  }
 
   render () {
     return (
@@ -32,7 +36,7 @@ export class YourRideOffersScreen extends Component {
           keyExtractor={item => item.rideId}
           renderItem={({ item }) => (<RideOffer ride={item} />)}
         />
-        <FloatingActionButton onPress={() => alert('Clicado')} />
+        <FloatingActionButton onPress={() => this.pushRideRequestScreen()} />
       </View>
 
     )
