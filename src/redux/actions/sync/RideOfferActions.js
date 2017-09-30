620a9ffe5facdde79ebb91e4004409b20d1358b2
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-30 09:14:39 -0300

    refactor: update flow types

--- src/redux/actions/sync/RideOfferActions.js
+++ src/redux/actions/sync/RideOfferActions.js
@@ -1,6 +1,7 @@
 import { FETCH_ALL_RIDE_OFFERS } from '../../types'
+import type { RideType } from '../../../services/firebase/types'
 
-export const updateRideOffers = (rides) => {
+export const updateRideOffers = (rides: Array<RideType>) => {
   return {
     type: FETCH_ALL_RIDE_OFFERS,
     payload: rides
--- src/redux/actions/sync/RideRequestActions.js
+++ src/redux/actions/sync/RideRequestActions.js
@@ -1,6 +1,7 @@
 import { FETCH_ALL_RIDE_REQUESTS } from '../../types/index'
+import type { RideRequestFlowType } from '../../../services/firebase/types'
 
-export const updateRideRequests = (rides) => {
+export const updateRideRequests = (rides: Array<RideRequestFlowType>) => {
   return {
     type: FETCH_ALL_RIDE_REQUESTS,
     payload: rides
