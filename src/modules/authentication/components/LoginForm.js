580bba1cea949b72327bca0cb61e77150c2f0e24
Eduardo Moroni <eduardomoroni@gmail.com>
2017-10-01 10:28:38 -0300

    feat: add lottie spinner

--- src/modules/authentication/components/LoginForm.js
+++ src/modules/authentication/components/LoginForm.js
@@ -5,6 +5,7 @@ import Toast, { DURATION } from 'react-native-easy-toast'
 import { RkText, RkAvoidKeyboard } from 'react-native-ui-kitten'
 import { GradientButton, TextInput } from '../../shared/components'
 import { styles } from '../containers/styles/LoginScreenStyles'
+import { LoadingSpinnerView } from '../../shared/components/LoadingSpinnerView'
 
 export class LoginForm extends React.Component {
   static propTypes = {
@@ -18,7 +19,7 @@ export class LoginForm extends React.Component {
 
   static defaultProps = {
     footer: () => null,
-    toast: { showAlert: false, message: '' }
+    toast: {showAlert: false, message: ''}
   }
 
   state = {
@@ -28,12 +29,12 @@ export class LoginForm extends React.Component {
   }
 
   onButtonPress = () => {
-    const { email, password } = this.state
+    const {email, password} = this.state
     this.props.onButtonPress(email, password)
   }
 
   componentWillReceiveProps (nextProps) {
-    const { showAlert, message } = nextProps.toast
+    const {showAlert, message} = nextProps.toast
 
     if (showAlert) {
       this.refs.toast.show(message, DURATION.LENGTH_LONG)
@@ -42,44 +43,47 @@ export class LoginForm extends React.Component {
 
   render () {
     return (
-      <RkAvoidKeyboard
-        style={styles.screen}
-        onStartShouldSetResponder={e => true}
-        onResponderRelease={e => Keyboard.dismiss()}>
+      <LoadingSpinnerView isLoading>
 
-        <View style={styles.header}>
-          <Image style={styles.image} source={require('../../../assets/images/caronaboard-logo-azul.png')} />
-          <RkText rkType='logo h0'>Caronaboard</RkText>
-          <RkText rkType='light h1'>Awesome Slogan</RkText>
-        </View>
+        <RkAvoidKeyboard
+          style={styles.screen}
+          onStartShouldSetResponder={e => true}
+          onResponderRelease={e => Keyboard.dismiss()}>
 
-        <TextInput
-          onChangeText={(email) => { this.setState({email}) }}
-          key='email-input'
-          rkType='rounded'
-          placeholder='Username'
-          autoCapitalize='none'
-        />
-        <TextInput
-          onChangeText={(password) => { this.setState({password}) }}
-          key='password-input'
-          rkType='rounded'
-          placeholder='Password'
-          secureTextEntry
-        />
+          <View style={styles.header}>
+            <Image style={styles.image} source={require('../../../assets/images/caronaboard-logo-azul.png')} />
+            <RkText rkType='logo h0'>Caronaboard</RkText>
+            <RkText rkType='light h1'>Awesome Slogan</RkText>
+          </View>
 
-        <View style={styles.centralized}>
-          <GradientButton
-            rkType='large'
-            style={styles.loginButton}
-            text={this.props.buttonText}
-            onPress={this.onButtonPress}
+          <TextInput
+            onChangeText={(email) => { this.setState({email}) }}
+            key='email-input'
+            rkType='rounded'
+            placeholder='Username'
+            autoCapitalize='none'
           />
-        </View>
+          <TextInput
+            onChangeText={(password) => { this.setState({password}) }}
+            key='password-input'
+            rkType='rounded'
+            placeholder='Password'
+            secureTextEntry
+          />
 
-        {this.props.footer()}
-        <Toast position='top' ref='toast' />
-      </RkAvoidKeyboard>
+          <View style={styles.centralized}>
+            <GradientButton
+              rkType='large'
+              style={styles.loginButton}
+              text={this.props.buttonText}
+              onPress={this.onButtonPress}
+            />
+          </View>
+
+          {this.props.footer()}
+          <Toast position='top' ref='toast' />
+        </RkAvoidKeyboard>
+      </LoadingSpinnerView>
     )
   }
 }
