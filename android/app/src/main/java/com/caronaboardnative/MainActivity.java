00c9fa7ff0ed353843327721390c4b6ecb405845
Eduardo Moroni <eduardomoroni@gmail.com>
2017-10-04 11:40:55 -0300

    feat: adiciona splash screen

--- android/app/src/main/java/com/caronaboardnative/MainActivity.java
+++ android/app/src/main/java/com/caronaboardnative/MainActivity.java
@@ -1,6 +1,29 @@
 package com.caronaboardnative;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#607D8B"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#FFFFFF"));
+        textView.setText("CaronaBoard");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
+    }
 }
